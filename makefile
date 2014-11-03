90e7c3f523bfedaea33710a7dd84cfe316df9101
Paul Kirth <pk1574@gmail.com>
2014-11-03 09:12:30 -0800

    Added skeleton file for project phase I
    Will be moving to branches for each phase, then merging everything back
    
    Probably will need new directory structure for everything...
    
    Wish this spec was out at the begining, so I could have planned for this...

--- makefile
+++ makefile
@@ -1,10 +1,13 @@
-all: server client ncd
+all: server client ncd skel
 
 ncd: ncd_main.c ncd.o
 	gcc ncd_main.c ncd.o -o ncd
 
 ncd.o: ncd.h ncd.c
 	gcc -c ncd.c 
+
+skel: skel.c
+	gcc skel.c -o skel
 
 server: server.c
 	gcc server.c -o server 
---
+++ skel.c
@@ -0,0 +1,77 @@
+/*
+*	COMP429 Fall 2014 Project Skeleton
+*/
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <string.h>
+
+
+/*  Just returns current time as double, with most possible precision...  */
+double get_time (void) {
+	struct timeval tv;
+	double d;
+	gettimeofday (&tv, NULL);
+	d = ((double) tv.tv_usec) / 1000000. + (unsigned long) tv.tv_sec;
+	return d;
+}
+
+
+uint16_t ip_checksum(void* vdata,size_t length) {
+    // Cast the data pointer to one that can be indexed.
+    char* data=(char*)vdata;
+
+    // Initialise the accumulator.
+    uint64_t acc=0xffff;
+
+    // Handle any partial block at the start of the data.
+    unsigned int offset=((uintptr_t)data)&3;
+    if (offset) {
+        size_t count=4-offset;
+        if (count>length) count=length;
+        uint32_t word=0;
+        memcpy(offset+(char*)&word,data,count);
+        acc+=ntohl(word);
+        data+=count;
+        length-=count;
+    }
+
+    // Handle any complete 32-bit blocks.
+    char* data_end=data+(length&~3);
+    while (data!=data_end) {
+        uint32_t word;
+        memcpy(&word,data,4);
+        acc+=ntohl(word);
+        data+=4;
+    }
+    length&=3;
+
+    // Handle any partial block at the end of the data.
+    if (length) {
+        uint32_t word=0;
+        memcpy(&word,data,length);
+        acc+=ntohl(word);
+    }
+
+    // Handle deferred carries.
+    acc=(acc&0xffffffff)+(acc>>32);
+    while (acc>>16) {
+        acc=(acc&0xffff)+(acc>>16);
+    }
+
+    // If the data began at an odd byte address
+    // then reverse the byte order to compensate.
+    if (offset&1) {
+        acc=((acc&0xff00)>>8)|((acc&0x00ff)<<8);
+    }
+
+    // Return the checksum in network byte order.
+    return htons(~acc);
+}
+
+
+int main (int arc, char *argv[]) {
+
+     return 0;
+}
