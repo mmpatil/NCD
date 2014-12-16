7ef0a6edb462efde2ab4ff77c033fcb98432dddb
Paul Kirth <pk1574@gmail.com>
2014-12-16 13:58:30 -0800

    Added beginings of unit testing
    This needs much more comprehensive unit tests to really be useful
    
    Must find a proper C unit test framework to use

---
+++ unit_test.c
@@ -0,0 +1,14 @@
+#include "ncd.h"
+
+
+int test_get_time(){ return 0; }
+
+int test_comp_det(){ return 0; }
+
+
+int test_send_data(){ return 0; }
+
+int test_rcv_data(){ return 0; }
+
+int test_ip_checksum(){ return 0; }
+
