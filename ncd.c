84bdf39a0d2444c716afd18a8a42d7cd2681d2ee
Paul Kirth <pk1574@gmail.com>
2014-12-23 13:25:30 -0800

    updated checksum access to BSD only

--- ncd.c
+++ ncd.c
@@ -299,7 +299,7 @@ int mkudphdr(void* buff, size_t udp_data_len, u_int8_t proto)
 	udp->uh_sport = htons(port); /* set source port*/
 	udp->uh_dport = htons(port); /* set destination port */
 	udp->uh_ulen = htons(udp_len); /* set udp length */
-	udp->check = 0;/* zero out the udp checksum */
+	udp->uh_sum = 0;/* zero out the udp checksum */
 
 	/*printf("ip: %d\n", ip);
 	 printf("udp, %d, udp+1: %d\n", udp, udp + 1);
@@ -323,7 +323,7 @@ int mkudphdr(void* buff, size_t udp_data_len, u_int8_t proto)
 
 	/* set udp checksum */
 
-	udp->check = ip_checksum(ps, udp_len + sizeof(struct pseudo_header));
+	udp->uh_sum = ip_checksum(ps, udp_len + sizeof(struct pseudo_header));
 
 	return 0;
 }
