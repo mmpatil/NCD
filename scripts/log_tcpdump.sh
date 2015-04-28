b4ced16ab03f88102cb6e2f7e9f81ad4784336aa
Paul Kirth <pk1574@gmail.com>
2015-04-28 01:46:37 -0700

    change to test script need to automate tcp listener

--- scripts/log_tcpdump.sh
+++ scripts/log_tcpdump.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 FILENAME=`./make_log.sh tcpdump`
-
+#nc -l -9876&
 #sudo iptables -A OUTPUT -p tcp --tcp-flags RST RST -j DROP
 
 ./test_runner.sh "sudo tcpdump -i any -v  -w $FILENAME"
 
 #sudo iptables -D OUTPUT -p tcp --tcp-flags RST RST -j DROP
+#kill %1
