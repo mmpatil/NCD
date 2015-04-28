0303bf737d9c530b5a198e8276b1fcd36a4b5e1c
Paul Kirth <pk1574@gmail.com>
2015-04-28 00:15:37 -0700

    updated scripts

--- scripts/tcp_test_runner.sh
+++ scripts/tcp_test_runner.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
-DIRNAME= ../$HOSTNAME/Test_Data/$DATE 
-DATE=date
-if[ -d $DIRNAME ]; then
-	COUNT=ls -afq | wc -l
+DATE="date +%F"
+DIRNAME=../test_data/$($hostname)/$($DATE)
+if [ -d "$DIRNAME" ]
+then
+	COUNT=`ls -afq | wc -l`
 else
 	COUNT=1
 	mkdir $DIRNAME
 fi
+echo $COUNT
 
-sudo iptables -A OUTPUT -p tcp --tcp-flags RST RST -j DROP
+#sudo iptables -A OUTPUT -p tcp --tcp-flags RST RST -j DROP
 
-./test_runner.sh "sudo tcpdump -i any -v" > $DIRNAME/$count
+./test_runner.sh "sudo tcpdump -i any -v" > $DIRNAME/"test_$COUNT"
 
-sudo iptables -D OUTPUT -p tcp --tcp-flags RST RST -j DROP
+#sudo iptables -D OUTPUT -p tcp --tcp-flags RST RST -j DROP
