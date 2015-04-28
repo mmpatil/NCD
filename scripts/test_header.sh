19afdfff3a5d5051ebd0a35eb7797d7408192762
vahab <vahab@nova.(none)>
2015-04-27 17:38:58 -0700

    Improved scripts for testing

--- scripts/test_header.sh
+++ scripts/test_header.sh
@@ -8,9 +8,8 @@ echo "HOSTNAME: $HOSTNAME"
 echo "USER: $USER"
 echo "COMMAND: $@"
 echo "TEST INFO:
-Test run from home machine, behind Time Warner NAT
-Some more relevant data
-The end ....\n"
+Test run from $HOSTNAME
+
 
 echo "ANALYSYS: \n"
 
