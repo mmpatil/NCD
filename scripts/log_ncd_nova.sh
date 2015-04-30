7877286e604ba0defef0a1d16b30ae0e74d1f7b3
Paul Kirth <pk1574@gmail.com>
2015-04-30 13:49:07 -0700

    fixed directory location in ncd scripts

--- scripts/log_ncd_nova.sh
+++ scripts/log_ncd_nova.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 FILENAME=`./make_log.sh ncd`
 sudo iptables -A OUTPUT -p tcp --tcp-flags RST RST -j DROP
-./test_runner.sh "sudo ./ncd_main 131.179.192.59 -p 9876 -n 50 -T" > $FILENAME
+./test_runner.sh "sudo ../ncd_main 131.179.192.59 -p 9876 -n 50 -T" > $FILENAME
 sudo iptables -D OUTPUT -p tcp --tcp-flags RST RST -j DROP
