fd70668a2042b1ddfa8fabf8825bef03d4df1555
Paul Kirth <pk1574@gmail.com>
2015-08-26 22:26:11 -0700

    Forgot to add the script

---
+++ travis_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+ 
+if [ $(CC) = "gcc" ] then
+    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+    sudo apt-get install -qq g++-4.8
+    export CXX="g++-4.8" 
+fi
+    sudo apt-get update -qq
+    sudo apt-get install libgtest-dev
+    cd /usr/src/gtest && sudo cmake .
+    sudo cmake --build .
+    sudo mv libg* /usr/local/lib/
+    cd -
+
