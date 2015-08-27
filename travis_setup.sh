5cb9b9e50b1f979ecd34f2972ae7d67b59272ee4
Paul Kirth <pk1574@gmail.com>
2015-08-26 22:42:02 -0700

    improve setup script for CI

--- travis_setup.sh
+++ travis_setup.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
- 
+
+set -e
+
 if [ "$CC" = "gcc" ] ; then
-    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-    sudo apt-get install -qq g++-4.8
-    export CXX="g++-4.8" 
-fi
+    sudo add-apt-repository ppa:ubuntu-toolchain-r/test
     sudo apt-get update -qq
+    sudo apt-get install -qq g++-4.8
+    export CXX="g++-4.8"
+    export CC="gcc-4.8"
+else
+    sudo apt-get update -qq
+fi
+
     sudo apt-get install libgtest-dev
     cd /usr/src/gtest && sudo cmake .
     sudo cmake --build .
