07a2420e6a999dac3ccd1863b12223d2ff00db95
Paul Kirth <pk1574@gmail.com>
2016-01-31 17:20:11 -0800

    Added more files to support package and unit tests

--- scripts/ScheduleManager/ScheduleManager.py
+++ scripts/ScheduleManager/ScheduleManager.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 import random, fileinput
 from datetime import datetime, date, time, timedelta
 from dateutil.rrule import rrule, DAILY
@@ -27,25 +28,30 @@ class ScheduleMaker(object):
         self.command = command
 
         # TODO read ip list from file
-        self.target_list = []
+        with open(ip_file) as f:
+            self.target_list = f.read().splitlines()
+
         self.output_file_name = "schedule.txt"
 
-    def create_schedule(self):
-        for day in per_delta(self.start_date, self.end_date, timedelta(days = 1)):
-            self.write_day(self.schedule_day(day))
+    def create_schedule(self, seed = None):
+        days = list(per_delta(self.start_date, self.end_date, timedelta(days = 1)))
+        with open(self.output_file_name, 'w') as outFile:
+            for day in days:
+                self.write_day(self.schedule_day(day, seed), outFile)
 
-    def schedule_day(self, my_date: date):
+    def schedule_day(self, my_date: date, seed = None):
         day = {}
-        d = datetime(my_date)
+        d = datetime.combine(my_date, time())
+        if seed is not None:
+            random.seed(seed)
+
         for period in per_delta(d,d+timedelta(days=1), self.period):
             # sample get me a random sample of the minutes in an hour ... up to 60
             # instead of putting that in the dictionary, we should put an actual date time
             # we should also pass in the day's date, and remove logic to make times consistent for the whole day
             # datetime removes a great deal of that calculation
 
-            # TODO: fix this function to use actual date objects correctly, will simplify much of the logic
-            # and relieve a great deal of testing
-            schedule = random.sample(per_delta(period, period + self. period, self.interval), len(self.target_list))
+            schedule = random.sample(list(per_delta(period, (period + self.period), self.interval)), len(self.target_list))
             #scheduled_times = [ datetime.combine(my_date, time(minute = minutes)) for minutes in schedule ]
 
             day.update(dict(zip(schedule, self.target_list)))
@@ -53,10 +59,23 @@ class ScheduleMaker(object):
         return day
 
 
-    def write_day(self,day):
-        with open(self.output_file_name) as outFile:
+    def write_day(self,day, outFile:fileinput):
+        """Write the schedule for the day to a file"""
+        #pprint(day)
+
+        items = [(k,v) for k,v in day.items()]
+        items.sort()
+        for line in items:
+            str = "{0}\t{1:15s}\t{2:s}\n".format(line[0],line[1], self.command)
+            outFile.write(str)
+
+            #pprint(items)
+            """
             for key in day:
-                outFile.write(key + "\t" + day[key] + "\n")
+                line = " {0}\t{1}".format(key, day[key])
+                pprint(line)
+                outFile.write(line)
+                """
 
 
 # taken from http://stackoverflow.com/questions/10688006/generate-a-list-of-datetimes-between-an-interval-in-python
@@ -65,3 +84,7 @@ def per_delta(start, end, delta):
     while curr < end:
         yield curr
         curr += delta
+
+if __name__ == "__main__":
+    scheduler = ScheduleMaker("ip_list.txt", date(2016, 3,15), date(2016, 3, 17),timedelta(hours=1), timedelta(minutes=1),"echo 'Hello World!'")
+    scheduler.create_schedule(0)
