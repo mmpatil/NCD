33f83fac332afccdacfdea59f272004d524ab6b8
Paul Kirth <pk1574@gmail.com>
2016-03-06 15:48:47 -0800

    missed the lack of '-y' on several consecutive commits!!!

--- travis_setup.sh
+++ travis_setup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-    sudo add-apt-repository ppa:george-edison55/cmake-3.x
+    sudo add-apt-repository -y ppa:george-edison55/cmake-3.x
 
 if [ "$CC" = "gcc" ] ; then
     sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
