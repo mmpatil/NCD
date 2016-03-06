c636c40a1b5a7cee2e35c5f51122910f133dcd3c
Paul Kirth <pk1574@gmail.com>
2016-03-06 15:45:59 -0800

    try to get travis not to get user imput with ppa

--- travis_setup.sh
+++ travis_setup.sh
@@ -2,22 +2,20 @@
 
 set -e
 
-
     sudo add-apt-repository ppa:george-edison55/cmake-3.x
 
 if [ "$CC" = "gcc" ] ; then
     sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
     sudo apt-get update -qq
-    sudo apt-get upgrade -qq
     sudo apt-get install -qq gcc-4.8-multilib g++-4.8-multilib
     export CXX="g++-4.8"
     export CC="gcc-4.8"
 else
     sudo apt-get update -qq
-    sudo apt-get upgrade -qq -y
 fi
 
-    sudo apt-get install libgtest-dev
+    sudo apt-get install cmake -y
+    sudo apt-get install libgtest-dev -y
     cd /usr/src/gtest && sudo cmake .
     sudo cmake --build .
     sudo mv libg* /usr/local/lib/
