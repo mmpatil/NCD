d44fe5d02897a5975a61dec824da894c0e15389b
Paul Kirth <pk1574@gmail.com>
2016-03-06 15:39:57 -0800

    modify travis setup to accomodate new ppa

--- travis_setup.sh
+++ travis_setup.sh
@@ -8,7 +8,7 @@ set -e
 if [ "$CC" = "gcc" ] ; then
     sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
     sudo apt-get update -qq
-    sudo apt-get upgrade -qq -y
+    sudo apt-get upgrade -qq
     sudo apt-get install -qq gcc-4.8-multilib g++-4.8-multilib
     export CXX="g++-4.8"
     export CC="gcc-4.8"
@@ -22,4 +22,3 @@ fi
     sudo cmake --build .
     sudo mv libg* /usr/local/lib/
     cd -
-
