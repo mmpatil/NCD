c509db5f203eee2a4f9d255ecbec230dd3990c77
Paul Kirth <pk1574@gmail.com>
2016-03-06 16:44:32 -0800

    one more fix for travis ... :(

--- travis_setup.sh
+++ travis_setup.sh
@@ -12,7 +12,7 @@ else
     sudo apt-get update -qq
 fi
 
-    sudo apt-get install cmake -y
+    
     sudo apt-get install libgtest-dev -y
     cd /usr/src/gtest && sudo cmake .
     sudo cmake --build .
