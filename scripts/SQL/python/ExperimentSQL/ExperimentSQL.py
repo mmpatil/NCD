2ae8fbdba04b1c7e478d56fc9d1ebce8e5f77685
Paul Kirth <pk1574@gmail.com>
2016-03-19 20:11:34 -0700

    Many updates to this sql script to automate the test running

--- scripts/SQL/python/ExperimentSQL/ExperimentSQL.py
+++ scripts/SQL/python/ExperimentSQL/ExperimentSQL.py
@@ -3,5 +3,38 @@ import MySQLdb
 
 def startExperiment():
     """start the experiment and get the experimental id"""
+    print "Starting Test...\n"
+    db = MySQLdb.connect(host="localhost", user="root",passwd="",db="testdb")
 
 
+    cursor = db.cursor()
+
+    cursor.execute("INSERT INTO `Experiments` () VALUES () ")
+
+    cursor.execute("SELECT LAST_INSERT_ID();")
+
+    res = cursor.fetchall()
+
+    expID = res[0][0]
+
+    print "Experiment ID: %d " % expID
+
+    cursor.callproc('pcap_insert', (expID, "new poop"))
+
+    #cursor.execute("select * from CommonData")
+    #cursor.execute('SELECT @_pcap_insert_0, @_pcap_insert_1')
+
+    res= cursor.fetchall()
+
+    pcapID = res[0][0]
+
+    print pcapID
+
+
+    print "\n...Ending Test"
+
+
+
+
+if __name__ == "__main__":
+    startExperiment()
