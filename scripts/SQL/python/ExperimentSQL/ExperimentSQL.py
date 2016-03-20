da711c915539d6f3391b14e74b9cacc55567d3b7
Paul Kirth <pk1574@gmail.com>
2016-03-19 20:57:18 -0700

    many sql scirpting changes, and travis config attempts

--- scripts/SQL/python/ExperimentSQL/ExperimentSQL.py
+++ scripts/SQL/python/ExperimentSQL/ExperimentSQL.py
@@ -1,11 +1,11 @@
-
+import subprocess
 import MySQLdb
+
 
 def startExperiment():
     """start the experiment and get the experimental id"""
     print "Starting Test...\n"
-    db = MySQLdb.connect(host="localhost", user="root",passwd="",db="testdb")
-
+    db = MySQLdb.connect(host="localhost", user="root", passwd="", db="testdb")
 
     cursor = db.cursor()
 
@@ -15,25 +15,27 @@ def startExperiment():
 
     res = cursor.fetchall()
 
+    db.commit()
+
     expID = res[0][0]
 
     print "Experiment ID: %d " % expID
 
+    # start the measurment client passed in from commandline ... or maybe it will use config file...
+    # subprocess.call(["detector",""])
+
     cursor.callproc('pcap_insert', (expID, "new poop"))
 
-    #cursor.execute("select * from CommonData")
-    #cursor.execute('SELECT @_pcap_insert_0, @_pcap_insert_1')
+    # cursor.execute("select * from CommonData")
+    # cursor.execute('SELECT @_pcap_insert_0, @_pcap_insert_1')
 
-    res= cursor.fetchall()
+    res = cursor.fetchall()
 
     pcapID = res[0][0]
 
     print pcapID
 
-
     print "\n...Ending Test"
-
-
 
 
 if __name__ == "__main__":
